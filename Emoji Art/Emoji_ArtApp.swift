4a198c888e86942b68fe5a9956a7a19818feb03e
Zeljko Lucic <zeljko.lucic99@gmail.com>
2023-12-22 19:45:52 +0100

    Initial commit

---
+++ Emoji Art/Emoji_ArtApp.swift
@@ -0,0 +1,17 @@
+//
+//  Emoji_ArtApp.swift
+//  Emoji Art
+//
+//  Created by Zeljko Lucic on 22.12.23..
+//
+
+import SwiftUI
+
+@main
+struct Emoji_ArtApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
