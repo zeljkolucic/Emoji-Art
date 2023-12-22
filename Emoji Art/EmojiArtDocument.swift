c36edf273e6ad7513c20980fad72a9fa9c14f128
Zeljko Lucic <zeljko.lucic99@gmail.com>
2023-12-22 20:51:48 +0100

    Implement drag and drop for background image

--- Emoji Art/EmojiArtDocument.swift
+++ Emoji Art/EmojiArtDocument.swift
@@ -10,7 +10,7 @@ import SwiftUI
 class EmojiArtDocument: ObservableObject {
     typealias Emoji = EmojiArt.Emoji
     
-    private var emojiArt = EmojiArt()
+    @Published private var emojiArt = EmojiArt()
     
     init() {
         emojiArt.addEmoji("🚲", at: .init(x: -200, y: -150), size: 200)
