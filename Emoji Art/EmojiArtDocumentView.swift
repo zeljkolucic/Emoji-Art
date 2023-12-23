4fcce5b258bc595620d5c0baa67b196c0905e758
Zeljko Lucic <zeljko.lucic99@gmail.com>
2023-12-23 11:16:01 +0100

    Move `ScrollingEmojis` view from `EmojiArtDocumentView` to `PaletteChooser` as it semantically makes more sense

--- Emoji Art/EmojiArtDocumentView.swift
+++ Emoji Art/EmojiArtDocumentView.swift
@@ -107,25 +107,6 @@ struct EmojiArtDocumentView: View {
     }
 }
 
-struct ScrollingEmojis: View {
-    let emojis: [String]
-    
-    init(_ emojis: String) {
-        self.emojis = emojis.uniqued.map(String.init)
-    }
-    
-    var body: some View {
-        ScrollView(.horizontal) {
-            HStack {
-                ForEach(emojis, id: \.self) { emoji in
-                    Text(emoji)
-                        .draggable(emoji)
-                }
-            }
-        }
-    }
-}
-
 #Preview {
     EmojiArtDocumentView(document: EmojiArtDocument())
         .environmentObject(PaletteStore(named: "Preview"))
--- Emoji Art/PaletteChooser.swift
+++ Emoji Art/PaletteChooser.swift
@@ -33,6 +33,25 @@ struct PaletteChooser: View {
     }
 }
 
+struct ScrollingEmojis: View {
+    let emojis: [String]
+    
+    init(_ emojis: String) {
+        self.emojis = emojis.uniqued.map(String.init)
+    }
+    
+    var body: some View {
+        ScrollView(.horizontal) {
+            HStack {
+                ForEach(emojis, id: \.self) { emoji in
+                    Text(emoji)
+                        .draggable(emoji)
+                }
+            }
+        }
+    }
+}
+
 #Preview {
     PaletteChooser()
         .environmentObject(PaletteStore(named: "Preview"))
